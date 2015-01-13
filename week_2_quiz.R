86d7fdff360e1a53c0c28d0b1c4f53d2d4a85294
jorge jardines <gardenunez@gmail.com>
2015-01-13 23:56:52 +0100

    small changes in week 2 quiz

--- week_2_quiz.R
+++ week_2_quiz.R
@@ -5,7 +5,21 @@ cube <- function(x, n)
 cube(3)
 
 x <- 1:10
-if(x[0] > 5){
-  x <- 1:40
+if(x > 5){
+  x <- 0
 }
 x
+
+f <- function(x){
+  g <- function(y){
+    y + z
+  }
+  z <- 4
+  x +g(x)
+}
+z <- 10
+f(3)
+
+x <- 5
+y <- if(x < 3){ NA } else { 10 }
+y
