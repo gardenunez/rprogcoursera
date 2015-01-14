f97e258db1ac2b480a2340a8daa141e9346fb123
jorge jardines <gardenunez@gmail.com>
2015-01-15 00:10:34 +0100

    enhance week 2 quiz script

--- week_2_quiz.R
+++ week_2_quiz.R
@@ -23,3 +23,18 @@ f(3)
 x <- 5
 y <- if(x < 3){ NA } else { 10 }
 y
+
+
+h <- function(x, y, = NULL, d = 3L){
+  z <- cbind(x, d)
+  if(!is.null(y))
+    z <- z + y
+  else
+    z <- z + f
+  g <- x + y / z
+  if (d == 3L)
+    return(g)
+  g <- g + 10
+  g
+}
+
