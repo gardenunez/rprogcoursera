c0b1a6d8d84e7be3808711f03e0a315f649740d8
jorge jardines <gardenunez@gmail.com>
2015-01-18 00:22:22 +0100

    implement corr function

--- corr.R
+++ corr.R
@@ -1,3 +1,4 @@
+source('utils.R')
 corr <- function(directory, threshold = 0){
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the csv files
@@ -8,7 +9,18 @@ corr <- function(directory, threshold = 0){
   ## nitrate and sulfate; the default is 0
   
   ## Return a numeric vector of correlations
-  
-  
+  filenames <- list.files(path = directory, 
+                          pattern = "*.csv",
+                          full.names=TRUE)
+  v <- c()
+  data <- joindf(directory)
+  for(i in 1:332){
+    df <- subset(data, ID == i)
+    sdf <- subset(df, !is.na(df$sulfate))
+    df <- subset(sdf, !is.na(sdf$nitrate))
+    if(nrow(df) > threshold){
+      v <- c(v,cor(df$nitrate, df$sulfate))
+    }
+  }
+  v
 }
-  
