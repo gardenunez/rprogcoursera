503f101668c9db6e3b7d36811eb0fdb1013badbc
jorge jardines <gardenunez@gmail.com>
2015-01-17 19:05:29 +0100

    add complete.R file

---
+++ complete.R
@@ -0,0 +1,17 @@
+complete <- function(directory, id = 1:332){
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the csv files
+  
+  ## 'pollutant' is a character vector of length 1 indicating
+  ## the name of the pollutant for which we will calculate the 
+  ## mean; either "sulfate" or "nitrate".
+  
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases 
+  
+}
