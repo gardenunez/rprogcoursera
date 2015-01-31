0974f7ca503ee6d94fa85ab578303ed05e11fc65
jorge jardines <gardenunez@gmail.com>
2015-01-31 21:35:57 +0100

    enhance the best function

--- best.R
+++ best.R
@@ -1,20 +1,7 @@
+source('utils.R')
 best <- function(state, outcome) {
     ## Read outcome data
-    data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-    outcomes <- c("heart attack", "heart failure", "pneumonia")
-    indices <- c(11,17,23)
-    
-    ##validate state and outcome
-    if(!state %in% data$State) 
-        stop("invalid state")
-    if(!outcome %in% outcomes)
-        stop("invalid outcome")
-    
-    ##filter the data
-    column <- indices[match(outcome, outcomes)]
-    data <- data[data$State == state, c(2, column)]
-    data[, 2] <- as.numeric(data[,2])
-    data <- na.omit(data)
+    data <- getOutcomeData(state, outcome)
     
     ## get min rate and candidate hospital. Sort by name and return the top 1
     minRate <- min(data[, 2])
